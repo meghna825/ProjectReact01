f2289aa53113538d8da13c37e278f84899671b77
Meghna Mathur <meghna825@gmail.com>
2023-12-14 08:53:35 +0530

    Chapter02:Igniting our App

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
 /* 
 const heading = React.createElement("h1",{},"Hello World from react")
 const root = ReactDOM.createRoot(document.getElementById('root'))
---
+++ README.md
@@ -0,0 +1,5 @@
+To start the application use parcel
+npx parcel index.html
+
+To create a build 
+npx parcel build index.html
