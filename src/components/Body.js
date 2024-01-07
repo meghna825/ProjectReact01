4705175ffc91beaf4114a91bcb2b59f593ce9987
Meghna Mathur <meghna825@gmail.com>
2024-01-07 08:23:38 +0530

    Chapter06:Exploring the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,14 @@
 import  '../../style.css';
 import RestuarantCard from './RestaurantCard';
-import restList from '../utils/mockData';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import Shimmer from './ShimmerUI';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Body = ()=>{
-    const [listOfRestaurants,setlistOfRestaurant] = useState(restList)
-    const [searchInput,setsearchInput] = useState(null)
+    const [listOfRestaurants,setlistOfRestaurant] = useState([])
+    const [allistOfRestaurants,setallistOfRestaurant] = useState([])
+    const [searchInput,setsearchInput] = useState('')
+    let allList = []
     // Normal JS
     let listOfRestaurantJS = [
         { 
@@ -44,10 +48,68 @@ const Body = ()=>{
                 "avgRating": 3.2}
         }
     ]
+    const filterSearch = (e,type = null)=>{
+        let filteredList = allistOfRestaurants
+        if(type == 'clear'){
+            setsearchInput('')
+            setlistOfRestaurant(allistOfRestaurants)
+        }else{
+            if(searchInput){
+                filteredList = filteredList.filter((item)=>{
+                    if(( item?.info?.name.toLowerCase().includes(searchInput.toLowerCase()))||
+                   (item?.info?.costForTwo.toLowerCase().includes(searchInput.toLowerCase()))){
+                            return item
+                        }  
+                })   
+            }
+            setlistOfRestaurant(filteredList)
+        }
+        
+    }
+    useEffect(()=>{
+        fetchData()
+    },[])
+    const fetchData = async ()=>{
+        try{
+            let  list = []
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&collection=99942&tags=&sortBy=&filters=&type=rcv2&offset=0&page_type=null')
+        const json = await data?.json()
+        if(json?.data?.cards.length>0){
+            const allresturant = json.data.cards
+             allresturant.map((item)=>{
+                if(item?.card?.card['@type'] == 'type.googleapis.com/swiggy.presentation.food.v2.Restaurant'){
+                    list.push(item.card.card)
 
-    return(
+                }
+            })
+        }
+        setallistOfRestaurant(list)
+        setlistOfRestaurant(list)
+        }catch(e){
+            console.log(e)
+            toast.error("Runtime error", {
+                // Set to 15sec
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 15000,
+            })
+        }
+        
+       
+    }
+
+    return allistOfRestaurants.length == 0?
+    <>
+        <Shimmer/>
+        <ToastContainer/>
+    </>:
+    (<>
         <div className='body'>
             <div className='filter'>
+            <div className ='search'id="search-container">
+                    <input type="text" id="search-input" placeholder="Enter your search term" value = {searchInput} onChange={(e)=>setsearchInput(e.target.value)}/>
+                    <button id="search-button" onClick= {(e)=>filterSearch(e)} style={{"marginRight":"10px"}}>Search</button>
+                    <button id="clear-button" onClick= {(e)=>filterSearch(e,'clear')}>Clear</button>
+            </div>
                 <button className='filter-btn' onClick={()=>{
                    const filteredList =  listOfRestaurants.filter((rest)=>
                          rest.info.avgRating>4.00
@@ -55,21 +117,18 @@ const Body = ()=>{
                     setlistOfRestaurant(filteredList)
                    
                 }}>Top Rated Button</button>
-                {/* <div id="search-container">
-                    <input type="text" id="search-input" placeholder="Enter your search term" value = {searchInput} onChange={(e)=>setsearchInput(e.target.value)}/>
-                    <button id="search-button" onClick= {(e)=>{
-
-                    }}>Search</button>
-            </div> */}
+                
             </div>
                 <div className='res-body'>
                         <h2 className='title'>What's on your mind?</h2>
                     <div className='res-container'>
-                        {listOfRestaurants.map(restuarant=><RestuarantCard key = {restuarant.id} resData = {restuarant}/>)}
+                        {listOfRestaurants.map(restuarant=><RestuarantCard key = {restuarant.info.id} resData = {restuarant}/>)}
                        
                     </div>
                 </div>
             </div>
+            <ToastContainer/>
+            </>
     )
 }
 export default Body
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import {LOGO_URL} from '../utils/constant';
 import  '../../style.css';
+import {useState} from 'react';
 
 const Header = ()=>{
+    const [btnNameReact,setbtnNameReact] = useState('Login')
+    console.log("Header Component")
     return (
         <div className='header'>
             <div className='logo'><img src={LOGO_URL}/></div>
@@ -11,6 +14,14 @@ const Header = ()=>{
                     <li>About</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className='login' onClick={()=>{
+                        if(btnNameReact == "login"){
+                            setbtnNameReact('logout')
+                        }else{
+                            setbtnNameReact('login')
+                        }
+                        
+                    }}>{btnNameReact}</button>
                 </ul> 
             </div>
 
